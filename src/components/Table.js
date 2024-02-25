1ee2f29d865f422f0c1c71d2bb8019a6f32e0390
lawass1864 <amwass4@hotmail.com>
2024-02-25 12:34:04 +0100

    tri des elements ok

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,71 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 const Table = ({ coinsData }) => {
-    // nombre de ligne qu'on va afficher
-    const [rangeNumber, SetRangeNumber] = useState(100)
-    return (
-        <div className="table-container">
-            <div className="table-header">
-                <div className="range-container">
-                    <span>Top {" "}
-                        {/* connexion de l'input avec le range avec la value={rangeNumber} */}
-                    <input type='text'
-                    value={rangeNumber} onChange={(e) => SetRangeNumber(e.target.value)} />
-                    </span>
-                    <input type="range" min="1" max="250" value={rangeNumber} onChange={(e) => SetRangeNumber(e.target.value)}/>
-                </div>
-            </div>
+  // nombre de ligne qu'on va afficher
+  const [rangeNumber, SetRangeNumber] = useState(100);
+
+  const [orderBy, setOrderBy] = useState("");
+  //  element  tete du tableau
+  const tableHeader = [
+    "Prix",
+    "MarketCap",
+    "Volume",
+    "1h",
+    "1j",
+    "1m",
+    "6m",
+    "1a",
+    "ATH",
+  ];
+
+  return (
+    <div className="table-container">
+      <div className="table-header">
+        <div className="range-container">
+          <span>
+            Top{" "}
+            {/* connexion de l'input avec le range avec la value={rangeNumber} */}
+            <input
+              type="text"
+              value={rangeNumber}
+              onChange={(e) => SetRangeNumber(e.target.value)}
+            />
+          </span>
+          <input
+            type="range"
+            min="1"
+            max="250"
+            value={rangeNumber}
+            onChange={(e) => SetRangeNumber(e.target.value)}
+          />
         </div>
-    );
+        {/* mapper les boutons de type radio*/}
+        {tableHeader.map((el) => (
+          <li key={el}>
+            <input
+              type="radio"
+              value={el}
+              name="header-el"
+              id={el}
+              defaultChecked={
+                // pour trier dans l'autre sens c'est "reverse"
+                el === orderBy || el === orderBy + "reverse" ? true : false
+              }
+              onClick={() => {
+                if (orderBy === el) {
+                  setOrderBy( el + "reverse")
+                } else {
+                setOrderBy(el) 
+              }
+              }}
+            />
+            {/*  on met les el */}
+            <label htmlFor={el}>{el}</label>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Table;
