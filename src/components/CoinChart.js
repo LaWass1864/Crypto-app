772acf52aaa8d576c4c661cb317fc0ab29b47be3
lawass1864 <amwass4@hotmail.com>
2024-02-29 18:34:28 +0100

    graphique ok

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -1,33 +1,32 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import {
-  Area,
   AreaChart,
-  CartesianGrid,
-  Tooltip,
   XAxis,
   YAxis,
+  CartesianGrid,
+  Tooltip,
+  Area,
 } from "recharts";
 import colors from "../styles/_settings.scss";
-//  ce composant va récuperer l'ID du coin qu'on passera en props.
 
 const CoinChart = ({ coinId, coinName }) => {
+  const [coinData, setCoinData] = useState();
   const [duration, setDuration] = useState(30);
-  const [coinData, setCoinData] = useState("");
-
   const headerData = [
     [1, "1 jour"],
     [3, "3 jours"],
     [7, "7 jours"],
     [30, "1 mois"],
     [91, "3 mois"],
-    [181, "3 mois"],
+    [181, "6 mois"],
     [365, "1 an"],
     [3000, "Max"],
   ];
 
   useEffect(() => {
     let dataArray = [];
+
     axios
       .get(
         `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=${duration}${
@@ -45,23 +44,21 @@ const CoinChart = ({ coinId, coinName }) => {
         }
         setCoinData(dataArray);
       });
-      // si la duration est rejouéé si elle est > 32 jours et le state va changer
-  }, [duration]);
+  }, [coinId, duration]);
 
   return (
     <div className="coin-chart">
-      {/* nom de la crypto */}
       <p>{coinName}</p>
       <div className="btn-container">
-        {headerData.map((el) => {
+        {headerData.map((radio) => {
           return (
             <div
-              key={el[0]}
-              htmlFor={"btn" + el[0]}
-              onClick={() => setDuration(el[0])}
-              className={el[0] === duration ? "active-btn" : " "}
+              htmlFor={"btn" + radio[0]}
+              onClick={() => setDuration(radio[0])}
+              key={radio[0]}
+              className={radio[0] === duration ? "active-btn" : ""}
             >
-              {el[1]}
+              {radio[1]}
             </div>
           );
         })}
